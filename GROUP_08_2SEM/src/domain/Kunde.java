76de149a387c1e204fba05303ea0b6ce6362c5e5
Jannik <Jannik@10.99.16.235>
2013-04-04 09:52:48 +0200

    heijagioj

--- GROUP_08_2SEM/src/domain/Kunde.java
+++ GROUP_08_2SEM/src/domain/Kunde.java
@@ -7,7 +7,10 @@ package domain;
 public class Kunde {
    private String kundeID;
    private String navn;
-
+   
+   public Kunde getKunde(){
+       return this;
+   }
     public Kunde(String navn) {
         this.navn = navn;
     }
