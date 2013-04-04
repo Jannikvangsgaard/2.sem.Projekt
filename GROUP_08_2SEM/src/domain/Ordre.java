3b0fa2bfd0e743b3c2ec128434676c4ccfd58714
Jannik <Jannik@10.99.16.235>
2013-04-04 10:17:02 +0200

    heijagioj

--- GROUP_08_2SEM/src/domain/Ordre.java
+++ GROUP_08_2SEM/src/domain/Ordre.java
@@ -10,21 +10,59 @@ public class Ordre {
     private int ordreNo;
     private int antal;
     private int kundeID;
+    private int state;
     private ArrayList<Vare> vareliste = new ArrayList<Vare>();
     public Ordre(int antal, int no, ArrayList<Vare> tissemand){
       this.antal = antal;
       this.ordreNo = no;
       vareliste = tissemand;          
+      state=0;        
               
-              
-              
-              
-      
-      
+    }
+
+    public int getOrdreNo() {
+        return ordreNo;
+    }
+
+    public void setOrdreNo(int ordreNo) {
+        this.ordreNo = ordreNo;
+    }
+
+    public int getAntal() {
+        return antal;
+    }
+
+    public void setAntal(int antal) {
+        this.antal = antal;
+    }
+
+    public int getKundeID() {
+        return kundeID;
+    }
+
+    public void setKundeID(int kundeID) {
+        this.kundeID = kundeID;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public ArrayList<Vare> getVareliste() {
+        return vareliste;
+    }
+
+    public void setVareliste(ArrayList<Vare> vareliste) {
+        this.vareliste = vareliste;
     }
     public ArrayList getVareArray(){
         return vareliste;
     }
+    
     
         
 }
