4e31ee0da3ee86cf3ea198f30bef164097bb9906
andersfriis91 <andersfriis91@10.99.19.248>
2013-04-05 13:03:44 +0200

    yolo $w4g

--- GROUP_08_2SEM/src/domain/Customer.java
+++ GROUP_08_2SEM/src/domain/Customer.java
@@ -12,10 +12,6 @@ public class Customer {
         this.customerID = customerID;
         this.name = name;
     }
-    public Customer(int customerID,String name){
-        this.customerID = customerID;
-        this.name=name;
-    }
 
     public int getCustomerID() {
         return customerID;
