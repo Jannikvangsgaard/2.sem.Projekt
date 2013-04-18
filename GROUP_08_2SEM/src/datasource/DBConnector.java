9058399bdaa2fcc489ef4ecf16116c98a3bdc111
andersfriis91 <andersfriis91@10.99.19.248>
2013-04-18 13:08:58 +0200

    horsedick.mpeg

--- GROUP_08_2SEM/src/datasource/DBConnector.java
+++ GROUP_08_2SEM/src/datasource/DBConnector.java
@@ -7,31 +7,40 @@ import java.sql.DriverManager;
  *
  * @author Jannik
  */
-public class DBConnector {
-     private static String id = "CLDATB12E8";
- private static String pw = "CLDATB12E8";
 
- public Connection getConnection() {
-  Connection con = null;
-  try {
-   Class.forName("oracle.jdbc.driver.OracleDriver");
-   con = DriverManager.getConnection(
-     "jdbc:oracle:thin:@delfi.lyngbyes.dk:1521:KNORD", id, pw);
-  } catch (Exception e) {
-   System.out
-     .println("\n*** Remember to insert your Oracle ID and PW in the DBConnector class! ***\n");
-   System.out.println("eror in DBConnector.getConnection()");
-   System.out.println(e);
-  }
+ public class DBConnector 
+ {
+    private static String id = "CLDATB12E8";
+    private static String pw = "CLDATB12E8";
 
-  return con;
- }
+    public Connection getConnection() 
+    {
+        Connection con = null;
+        try 
+        {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            con = DriverManager.getConnection(
+                "jdbc:oracle:thin:@delfi.lyngbyes.dk:1521:KNORD", id, pw);
+        } 
+        catch (Exception e) 
+        {
+            System.out
+            .println("\n*** Remember to insert your Oracle ID and PW in the DBConnector class! ***\n");
+            System.out.println("eror in DBConnector.getConnection()");
+            System.out.println(e);
+        }
+        return con;
+    }
 
- public void releaseConnection(Connection con) {
-  try {
-   con.close();
-  } catch (Exception e) {
-   System.err.println(e);
-  }
- }
+    public void releaseConnection(Connection con) 
+    {
+        try 
+        {
+            con.close();
+        } 
+        catch (Exception e) 
+        {
+            System.err.println(e);
+        }
+    }  
 }
